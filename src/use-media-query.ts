363400d1bba29b9f3e8b374ca1fde11fdd59b1d3
Adam Bergman <adam@fransvilhelm.com>
2019-02-06 10:13:18 +0100

    docs(useMediaQuery): Document usage of useMediaQuery

--- src/use-media-query.ts
+++ src/use-media-query.ts
@@ -1,5 +1,16 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * Test to see if a query is matched and listen for changes on that query. This
+ * value will be updated if the window is resized.
+ *
+ * @example
+ *   const isMobile = useMediaQuery('(max-width: 375px)');
+ *   const isTablet = useMediaQuery('(min-width: 378px) and (max-width: 768px)');
+ *
+ * @param {string} query
+ * @returns {boolean}
+ */
 const useMediaQuery = (query: string): boolean => {
   const [matches, setMatches] = useState(
     () => window.matchMedia(query).matches,
