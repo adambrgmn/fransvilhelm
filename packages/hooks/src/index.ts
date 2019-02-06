eca7510f69cb8ce07134fcf6e61aac7c8c35085b
Adam Bergman <adam@fransvilhelm.com>
2019-02-06 08:01:24 +0100

    fix(hooks): Add useMediaQuery export

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,4 @@
 export { useInterval } from './use-interval';
+export { useMediaQuery } from './use-media-query';
 export { useOnline } from './use-online';
 export { useScrollPosition } from './use-scroll-position';
