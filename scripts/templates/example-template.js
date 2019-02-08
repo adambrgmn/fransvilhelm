f7b7b0b15b3a87d3069d0deb40f9275053f723b0
Adam Bergman <adam@fransvilhelm.com>
2019-02-08 14:04:44 +0100

    chore: Update template creation after restructure

--- scripts/templates/example-template.js
+++ scripts/templates/example-template.js
@@ -1,13 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { toCamelCase, toKebabCase, toStudlyCaps } = require('strman');
+const { toCamelCase, toStudlyCaps } = require('strman');
 
 module.exports = ({ name }) => {
   const camel = toCamelCase(name);
-  const kebab = toKebabCase(name);
   const studly = toStudlyCaps(name);
 
   return `import * as React from 'react';
-import { ${camel} } from '../../${kebab}';
+import { ${camel} } from './';
 
 const ${studly}Example = (): JSX.Element => {
   const result = ${camel}();
