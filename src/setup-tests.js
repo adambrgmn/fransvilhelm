0320676119234d8cc9aba9ac256628de185eab31
Adam Bergman <adam@fransvilhelm.com>
2019-02-11 15:48:23 +0100

    feat: Add useInView hook (#11)
    * test: Setup IntersectionObserver mock in test env
    
    * feat(hook): Add useInView hook

---
+++ src/setup-tests.js
@@ -0,0 +1,36 @@
+const observers = [];
+
+class IntersectionObserver {
+  static emit(baseEntry) {
+    observers.forEach(observer => {
+      const targets = observer.targets.map(target => ({
+        target,
+        ...baseEntry,
+      }));
+
+      observer.handler(targets, observer);
+    });
+  }
+
+  constructor(handler) {
+    this.handler = handler;
+    this.targets = [];
+
+    this.observe = jest.fn(this.observe.bind(this));
+    this.unobserve = jest.fn(this.unobserve.bind(this));
+
+    observers.push(this);
+  }
+
+  observe(target) {
+    this.targets.push(target);
+  }
+
+  unobserve(target) {
+    this.targets = this.targets.filter(e => e !== target);
+  }
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  value: IntersectionObserver,
+});
