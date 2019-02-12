b1b1d32ca381a99cffc4945a82dbb968a34b3175
Adam Bergman <adam@fransvilhelm.com>
2019-02-12 08:25:25 +0100

    fix(use-lock-body-scroll): Make hook accept a container to lock scroll

--- src/use-lock-body-scroll/use-lock-body-scroll.example.tsx
+++ src/use-lock-body-scroll/use-lock-body-scroll.example.tsx
@@ -3,13 +3,16 @@ import { useCheckbox } from '../use-checkbox';
 import { useLockBodyScroll } from './';
 
 const UseLockBodyScrollExample = (): JSX.Element => {
+  const ref = React.useRef<HTMLDivElement>(null);
   const checkbox = useCheckbox(false);
-  useLockBodyScroll(checkbox.checked);
+  useLockBodyScroll(checkbox.checked, ref);
 
   return (
-    <div>
-      <input type="checkbox" id="checkbox" {...checkbox} />{' '}
-      <label htmlFor="checkbox">Lock body scroll</label>
+    <div ref={ref} style={{ height: 200, overflow: 'scroll' }}>
+      <div style={{ height: '100vh' }}>
+        <input type="checkbox" id="checkbox" {...checkbox} />{' '}
+        <label htmlFor="checkbox">Lock body scroll</label>
+      </div>
     </div>
   );
 };
--- src/use-lock-body-scroll/use-lock-body-scroll.test.tsx
+++ src/use-lock-body-scroll/use-lock-body-scroll.test.tsx
@@ -17,3 +17,21 @@ it('should lock scrolling of body when enabled', () => {
   render(<TestComponent lock={false} />, { container });
   expect(document.body.style.overflow).toEqual('visible');
 });
+
+it('should accept a second argument to pick a target a specific element', () => {
+  const Comp = (): JSX.Element => {
+    const ref = React.useRef<HTMLDivElement>(null);
+    useLockBodyScroll(true, ref);
+
+    return (
+      <div data-testid="container" ref={ref}>
+        <p>Hello world</p>
+      </div>
+    );
+  };
+
+  const { getByTestId } = render(<Comp />);
+  const container = getByTestId('container');
+
+  expect(container.style.overflow).toEqual('hidden');
+});
