5aac80e824511190db6b01c50234e63401351408
Adam Bergman <adam@fransvilhelm.com>
2020-02-23 17:15:26 +0100

    feat(use-dimensions): accept ref as input
    Move from accepting ref as input instead of as before where it came out as an output. This will
    enable greater composition of refs for the user.
    
    BREAKING CHANGE: Require ref as input instead as output

--- src/use-dimensions/use-dimensions.test.tsx
+++ src/use-dimensions/use-dimensions.test.tsx
@@ -5,7 +5,8 @@ import { useDimensions } from './';
 afterEach(cleanup);
 
 const TestComponent = (): JSX.Element => {
-  const { ref, rect } = useDimensions<HTMLParagraphElement>();
+  const ref = React.useRef<HTMLParagraphElement>(null);
+  const rect = useDimensions(ref);
   return <p ref={ref}>{rect && rect.width}</p>;
 };
 
