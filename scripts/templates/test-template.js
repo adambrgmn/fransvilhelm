5de45b78e45b58844b68e109ba1f6cb9ff64db33
Adam Bergman <adam@fransvilhelm.com>
2020-02-23 17:21:14 +0100

    chore: update templates

--- scripts/templates/test-template.js
+++ scripts/templates/test-template.js
@@ -2,12 +2,10 @@
 const { toCamelCase } = require('strman');
 
 module.exports = ({ name }) => `import * as React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { ${toCamelCase(name)} } from './';
 
-afterEach(cleanup);
-
-const TestComponent = (): JSX.Element => {
+const TestComponent: React.FC = () => {
   const result = ${toCamelCase(name)}();
   return <p />;
 };
