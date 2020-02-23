4e3846ab5b7ca766df736bf7114c54f8ce10adf0
Adam Bergman <adam@fransvilhelm.com>
2020-02-23 23:41:37 +0100

    chore: setup plop for bootrapping

--- scripts/templates/hook-template.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { toCamelCase } = require('strman');
-
-module.exports = ({ name }) => `import {} from 'react';
-
-const ${toCamelCase(name)} = () => {};
-
-export { ${toCamelCase(name)} };
-`;
