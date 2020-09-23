7a9307cec55177779600b8aba6c9ce3b0b1adb6e
Adam Bergman <adam@fransvilhelm.com>
2020-09-23 20:19:06 +0200

    Add useCLickOutside hook (#24)

---
+++ src/use-click-outside/index.ts
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from 'react';
+
+const useClickOutside = (
+  ref: React.RefObject<HTMLElement>,
+  handler: (event: Event) => void,
+) => {
+  let handlerRef = useRef(handler);
+
+  useEffect(() => {
+    handlerRef.current = handler;
+  });
+
+  useEffect(() => {
+    let listener = (event: Event) => {
+      if (
+        ref.current &&
+        event.target instanceof Node &&
+        !ref.current.contains(event.target)
+      ) {
+        handlerRef.current(event);
+      }
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [handler, ref]);
+};
+
+export { useClickOutside };
