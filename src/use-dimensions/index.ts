f1dc7282729d6a0e08e42921e949d22592f9967e
Adam Bergman <adam@fransvilhelm.com>
2021-12-03 23:47:58 +0100

    Remove unused resize observer refs

--- src/use-dimensions/index.ts
+++ src/use-dimensions/index.ts
@@ -4,12 +4,7 @@ import { observeRect, useIsomorphicLayoutEffect } from '../utils';
 
 /**
  * Get the current measurements of an element in the document. It will also
- * listen for resize changes. Note that it's most performant using
- * [ResizeObserver](https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver)
- * but this api is not available in all browsers. In that case it will listen
- * for the resize-event on the window, but this will not take every type of
- * resize into account, since an element can be resized in other ways than that
- * the window gets resized. In that case you have to mock ResizeObserver.
+ * listen for resize changes.
  *
  * @param ref React ref object containing an Element
  * @param observe Boolean flag indicating wether to observe changes or not
