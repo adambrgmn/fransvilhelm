f013cc7d0f090942590b040c34ed1637b4b66a2b
Adam Bergman <adam@fransvilhelm.com>
2021-12-08 10:32:33 +0100

    Remove unused variable

--- src/use-proxy-ref.ts
+++ src/use-proxy-ref.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useMemo } from 'react';
+import React, { useRef, useMemo } from 'react';
 
 import { InternalRefArg } from './types';
 import { assignRef } from './utils';
