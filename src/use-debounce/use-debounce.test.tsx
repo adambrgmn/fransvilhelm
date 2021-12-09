dfffbd78e3b5c4e423783975da354d2eb0bd21b5
Adam Bergman <adam@fransvilhelm.com>
2021-12-09 20:01:50 +0100

    Remove unused

--- src/use-debounce/use-debounce.test.tsx
+++ src/use-debounce/use-debounce.test.tsx
@@ -1,4 +1,4 @@
-import { render, act, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { useDebounce } from './use-debounce';
