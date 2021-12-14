1f92f99a8f22882e12d8462f7a653efbce02a18a
Adam Bergman <adam@fransvilhelm.com>
2021-12-14 09:47:37 +0100

    Update monorepo config

--- packages/hooks/jest.config.js
+++ packages/hooks/jest.config.js
@@ -4,11 +4,5 @@ module.exports = {
     '@testing-library/jest-dom',
     '<rootDir>/src/setup-tests.ts',
   ],
-  testPathIgnorePatterns: ['/node_modules/', '/.docz/'],
-  collectCoverageFrom: [
-    '**/*.{ts,tsx}',
-    '!src/index.ts',
-    '!**/*.example.tsx',
-    '!**/*.d.ts',
-  ],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!src/index.ts', '!**/*.d.ts'],
 };
