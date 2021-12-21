56eacfaf3810bc19c03f94879a9e0cc5c36d51c5
Adam Bergman <adam@fransvilhelm.com>
2021-12-21 21:20:50 +0100

    Setup storybook a11y addon

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
   framework: '@storybook/react',
   babel: (config) => {
     config.plugins.push(require.resolve('babel-plugin-istanbul'));
